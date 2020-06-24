8ea0abafa0d67bd8e1869131682d41f6e0638e22
Nikunj295 <nikunj.viradiya.160410116144@gmail.com>
2020-06-24 12:19:30 +0530

    GET/params --third commit

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, url_for
-from flask import request
+from flask import request,redirect
 from flask import jsonify, make_response
 
 from sklearn.datasets import make_blobs
-# from matplotlib import pyplot
 import pandas as pd
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split    
@@ -17,21 +16,27 @@ def createData():
     y=pd.DataFrame(y)
     return X,y
 
-
-def normalize_query_param(value):
+def getMultipleValues(value):
     return value if len(value) > 1 else value[0]
-
 
 def get_params(params):
     params_non_flat = params.to_dict(flat=False)
-    temp = {k: normalize_query_param(v) for k, v in params_non_flat.items()}
+    temp = {k: getMultipleValues(v) for k, v in params_non_flat.items()}
     end = int(params["end"]) if 'end' in temp else None
     start = int(params["start"]) if 'start' in temp else None
-    return start,end
+    type_of_algo = params["algorithm"] if 'start' in temp else "linear"
+    return start,end,type_of_algo
 
+@app.route('/selectAlgorithm')
+def selectAlgorithm():
+    start,end, type_of_algo = get_params(request.args)    
+    if type_of_algo=="linear":
+        return redirect(url_for('linearRegression'))
+    elif type_of_algo=="logisticRegression":
+        return redirect(url_for('logisticRegression'))
 
 @app.route('/linearRegression')
-def linear():
+def linear(start,end,type_of_algo):
     X,y = createData()
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=0)
     model = linear_model.LinearRegression()
@@ -41,8 +46,8 @@ def linear():
     X_test['Predicted'] = y_pred
     X_test['Original'] = y_test
 
-    # start,end = get_params(request.args)
-    # X_test = X_test[start:end][['Original','Predicted']]
+
+    X_test = X_test[start:end][['Original','Predicted']]
     return X_test.to_json(orient="index")
 
 
@@ -56,12 +61,13 @@ def logistic():
     y_pred = pd.DataFrame(y_pred)
     X_test['Predicted'] = y_pred
     X_test['Original'] = y_test
-    SCORE = jsonify(r2_score(y_test, y_pred))
+    # SCORE = jsonify(r2_score(y_test, y_pred))
     
     
     ##### HANDLE PARAMS #####
-    start,end = get_params(request.args)
+    start, end, type_of_algo = get_params(request.args)
     X_test = X_test[start:end][['Original','Predicted']]
+    
     
     ##### Output #####
     return X_test.to_json(orient='index')
